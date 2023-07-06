328deae53c4ea5de2d0d6c2091a6061c457bcf72
dpluan <dpluan2025@gmail.com>
2023-07-06 08:05:16 +0700

    u

--- settings.py
+++ settings.py
@@ -26,8 +26,8 @@ SOURCES_LIST = [IMAGE, VIDEO, WEBCAM, YOUTUBE]
 
 # Images config
 IMAGES_DIR = ROOT / 'images'
-DEFAULT_IMAGE = IMAGES_DIR / 'default_detected.jpg'
-DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'default_detected1.jpg'
+DEFAULT_IMAGE = IMAGES_DIR / 'md.jpg'
+DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'mdd.jpg'
 
 # Videos config
 VIDEO_DIR = ROOT / 'videos'
